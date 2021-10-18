d06a85a5e5902966a551c7c50bf9f3693d778a09
reneselander <50113423+reneselander@users.noreply.github.com>
2021-10-18 13:58:09 +0300

    Working ground

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 -70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200, 200, 200)
+
+ball_speed_x = 2
+ball_speed_y = 2
+
+
+
 '''
 Every manouver by user is called events
 '''
@@ -17,5 +29,14 @@ while True:
             pygame.quit()
             sys.exit()
 
-pygame.display.flip()
-clock.tick(60)
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2, screen_height))
+
+    pygame.display.flip()
+    clock.tick(60)
